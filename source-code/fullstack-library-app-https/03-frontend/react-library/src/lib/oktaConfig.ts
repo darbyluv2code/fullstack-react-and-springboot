6ce20d7935faa29a8f007c80937c71eaa828892d
Eric Roby <ericroby@Erics-MacBook-Pro.local>
2023-02-13 16:20:27 -0500

    init

--- source-code/fullstack-library-app-https/03-frontend/react-library/src/lib/oktaConfig.ts
+++ source-code/fullstack-library-app-https/03-frontend/react-library/src/lib/oktaConfig.ts
@@ -1,7 +1,7 @@
 export const oktaConfig = {
     clientId: '<CLIENTID HERE>',
     issuer: 'https://dev-<DEV ID>.okta.com/oauth2/default',
-    redirectUri: 'http://localhost:3000/login/callback',
+    redirectUri: 'https://localhost:3000/login/callback',
     scopes: ['openid', 'profile', 'email'],
     pkce: true,
     disableHttpsCheck: true,
